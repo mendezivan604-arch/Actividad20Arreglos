8ebe86e3d860d1921977d588e6cf6a7d22886c4c
Ivan mendez <mendezivan604@gmail.com>
2026-04-09 18:12:05 -0600

    Ejercicio 1

--- Actividad20Arreglos/Program.cs
+++ Actividad20Arreglos/Program.cs
@@ -1,1 +1,32 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Ejercicio #1");
+int cantidadnumeros;
+
+while (true)
+{
+    Console.Write("Ingrese cantidad de numeros: ");
+    if (int.TryParse(Console.ReadLine(), out cantidadnumeros) && cantidadnumeros > 0)
+    {
+        break;
+    }
+    Console.WriteLine("Cantidad de numeros inválida");
+}
+
+int[] numeros = new int[cantidadnumeros];
+for (int i = 0; i < numeros.Length; i++)
+{
+    while (true)
+    {
+        Console.Write("Ingrese numero: ");
+        if (int.TryParse(Console.ReadLine(), out numeros[i]) && numeros[i] > 0)
+        {
+            break;
+        }
+        Console.WriteLine("Número inválida");
+    }
+
+}
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine(numeros[i]);
+}
