5afa48c958a740e0c3a33437cc5c5dd3fdcf2320
Ivan mendez <mendezivan604@gmail.com>
2026-04-09 22:09:56 -0600

    Ejercicio 10

---
+++ Ejercicio_10/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Ejercicio #8");
+int[] numeros = { 5, 7, 8, 8};
+int buscado;
+Console.Write("Ingrese el valor a contar: ");
+buscado = int.Parse(Console.ReadLine());
+
+int contador = 0;
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    if (numeros[i] == buscado)
+    {
+        contador++;
+    }
+}
+
+Console.WriteLine($"El número {buscado} se repite {contador} veces");
