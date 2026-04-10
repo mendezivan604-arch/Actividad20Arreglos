76ff97d0d40cf0c1e24807488b89e98f0f7c25bb
Ivan mendez <mendezivan604@gmail.com>
2026-04-09 20:29:27 -0600

    Ejercicio 9

---
+++ Ejercicio_9/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.WriteLine("Ejercicio #9");
+int cantidadnumeros;
+while (true)
+{
+    Console.Write("Ingrese la cantidad de números: ");
+    if (int.TryParse(Console.ReadLine(), out cantidadnumeros) && cantidadnumeros > 0)
+    {
+        break;
+    }
+    Console.WriteLine("Cantidad inválida");
+}
+
+int[] numeros = new int[cantidadnumeros];
+
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    while (true)
+    {
+        Console.Write($"Ingrese número: ");
+        if (int.TryParse(Console.ReadLine(), out numeros[i]))
+        {
+            break;
+        }
+        Console.WriteLine("Número inválido");
+    }
+}
+
+int buscado;
+Console.Write("Ingrese el número a buscar: ");
+buscado = int.Parse(Console.ReadLine());
+
+bool encontrado = false;
+
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    if (numeros[i] == buscado)
+    {
+        Console.WriteLine($"Número encontrado en la posición {i}");
+        encontrado = true;
+        break;
+    }
+}
+
+if (!encontrado)
+{
+    Console.WriteLine("Número no encontrado");
+}
