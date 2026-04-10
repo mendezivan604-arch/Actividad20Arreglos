a6a933caf93163602577087a09ecab239afaae06
Ivan mendez <mendezivan604@gmail.com>
2026-04-09 20:24:39 -0600

    Ejercicio 8

---
+++ Ejercicio_8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Ejercicio #8");
+int[] numeros = { 5, 7, 8, 9};
+int contadorImpares = 0;
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    if (numeros[i] % 2 != 0)
+    {
+        contadorImpares++;
+    }
+}
+
+Console.WriteLine($"Cantidad de números impares: {contadorImpares}");
