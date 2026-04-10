dd0ce6695e7afdce59991dbf4d48aeecc602dc1e
Ivan mendez <mendezivan604@gmail.com>
2026-04-09 19:30:36 -0600

    Ejercicio 4

---
+++ Ejercicio_4/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Ejercicio #4");
+int cantidadnumeros;
+double suma = 0;
+double promedio;
+while (true)
+{
+    Console.Write("Ingrese cantidad de numeros: ");
+    if (int.TryParse(Console.ReadLine(), out cantidadnumeros) && cantidadnumeros > 0)
+    {
+        break;
+    }
+    Console.WriteLine("Cantidad de numeros inválida");
+}
+
+int[] numeros = new int[cantidadnumeros];
+for (int i = 0; i < numeros.Length; i++)
+{
+    while (true)
+    {
+        Console.Write("Ingrese numero: ");
+        if (int.TryParse(Console.ReadLine(), out numeros[i]) && numeros[i] > 0)
+        {
+            break;
+        }
+        Console.WriteLine("Número inválida");
+    }
+    suma += numeros[i];
+}
+promedio = suma / cantidadnumeros;
+Console.WriteLine($"El promedio es: {promedio}");
