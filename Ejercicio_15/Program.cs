100a83e587ef5c7aeda1e2740ef2cbe6c128ba8c
Ivan mendez <mendezivan604@gmail.com>
2026-04-09 23:02:53 -0600

    Ejercicio 15

---
+++ Ejercicio_15/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Ejercicio #10");
+int[] numeros = { 5, 7, 8, 8 };
+Console.WriteLine("Números pares:");
+for (int i = 0; i < numeros.Length; i++)
+{
+    if (numeros[i] % 2 == 0)
+    {
+        Console.WriteLine(numeros[i]);
+    }
+}
+
+Console.WriteLine("Números impares:");
+for (int i = 0; i < numeros.Length; i++)
+{
+    if (numeros[i] % 2 != 0)
+    {
+        Console.WriteLine(numeros[i]);
+    }
+}
