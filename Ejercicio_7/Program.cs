6d5989fb082f177d620813c88e2f1e00919bddb3
Ivan mendez <mendezivan604@gmail.com>
2026-04-09 20:21:48 -0600

    Ejercicio_7

---
+++ Ejercicio_7/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Ejercicio #7");
+int[] numeros = {5, 7, 8, 10};
+int contadorPares = 0;
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    if (numeros[i] % 2 == 0)
+    {
+        contadorPares++;
+    }
+}
+
+Console.WriteLine($"Cantidad de números pares: {contadorPares}");
